f3678df24199f1487e7b03448c07c064510057a0
E.8556 <E.8556@T4NISHI>
2018-07-16 01:27:29 +0900

    [add] fix all notes

---
+++ NE4S/Notes/Hold.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NE4S.Notes
+{
+    public class Hold : LongNote
+    {
+        public Hold()
+        {
+
+        }
+    }
+}
