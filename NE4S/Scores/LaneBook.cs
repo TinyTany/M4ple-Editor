0b05320d73aee039bdb02621ec67aa03df4d4c58
t4nishi <t4nishi.x@gmail.com>
2018-09-02 22:21:33 +0900

    Revert "Lane用リストLaneBook追加"
    This reverts commit 9ee8efbfbc234a11af408869307e8a9147101162.

--- NE4S/Scores/LaneBook.cs
+++ NE4S/Scores/LaneBook.cs
@@ -6,28 +6,15 @@ using System.Threading.Tasks;
 
 namespace NE4S.Scores
 {
-    public class LaneBook : List<ScoreLane>
+    public class LaneBook
     {
+        private List<ScoreLane> lanes;
+
         public LaneBook()
         {
-            
+            lanes = new List<ScoreLane>();
         }
 
-        /// <summary>
-        /// 指定されたlaneの次の要素を返します
-        /// </summary>
-        /// <param name="lane"></param>
-        /// <returns></returns>
-        public ScoreLane Next(ScoreLane lane)
-        {
-            if(!Contains(lane) || IndexOf(lane) == Count - 1)
-            {
-                return null;
-            }
-            else
-            {
-                return this.ElementAt(IndexOf(lane) + 1);
-            }
-        }
+
     }
 }
