1a0d26915d6c1d245d9ab1f1c3ec6da5cf64d12c
t4nishi <t4nishi.x@gmail.com>
2018-09-03 18:11:09 +0900

    Revert "Revert "カスタム挿入用フォーム追加""
    This reverts commit e660684e9ae820f49b0c7bdfcfc7cdfc1be90be6.

---
+++ NE4S/Form/BarAddCustomForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NE4S.Form
+{
+    public partial class BarAddCustomForm : Form
+    {
+        public BarAddCustomForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
