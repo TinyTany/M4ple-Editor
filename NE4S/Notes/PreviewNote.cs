83c633a369296f448b3a626b2162d37259a65db6
E.8556 <t4nishi.x@gmail.com>
2018-09-12 01:01:02 +0900

    プレビューノーツ用クラス追加

---
+++ NE4S/Notes/PreviewNote.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using NE4S.Scores;
+
+namespace NE4S.Notes
+{
+    public class PreviewNote
+    {
+        private RectangleF note;
+
+        public PreviewNote()
+        {
+            note = new RectangleF();
+        }
+
+        public void Paint(PaintEventArgs e)
+        {
+            note.Size = new SizeF(ScoreInfo.LaneWidth * Status.NoteSize, ScoreInfo.NoteHeight);
+            e.Graphics.FillRectangle(Brushes.Red, note);
+        }
+
+        public PointF Location
+        {
+            get { return note.Location; }
+            set { note.Location = value; }
+        }
+    }
+}
