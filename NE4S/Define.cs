0c84ea88e5ddb7b5aa45e45cfbc6eb33c59b9e94
E.8556 <t4nishi.x@gmail.com>
2018-09-12 01:01:21 +0900

    ノーツ種別の定数追加

--- NE4S/Define.cs
+++ NE4S/Define.cs
@@ -9,5 +9,21 @@ namespace NE4S
     public class Define
     {
         public const int ADD = 0, EDIT = 1, DELETE = 2;
+        public const int
+            TAP = 0,
+            EXTAP = 1,
+            AWEXTAP = 2,
+            FLICK = 3,
+            HELL = 4,
+            HOLD = 5,
+            SLIDE = 6,
+            SLIDECURVE = 9,
+            AIRUPC = 10,
+            AIRUPL = 11,
+            AIRUPR = 12,
+            AIRDOWNC = 13,
+            AIRDOWNL = 14,
+            AIRDOWNR = 15,
+            AIRHOLD = 16;
     }
 }
