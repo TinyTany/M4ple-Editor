62e1321561fd02406a1717a360e764e7fc3f617f
E.8556 <t4nishi.x@gmail.com>
2018-09-20 03:14:10 +0900

    MyUtilクラス追加

---
+++ NE4S/MyUtil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NE4S
+{
+	public class MyUtil
+	{
+		public static uint GCD(uint m, uint n)
+		{
+			if (m < n) return GCD(n, m);
+			if (m == 0 && n == 0) return 1;
+			if (n == 0) return m;
+			return GCD(n, m % n);
+		}
+	}
+}
