21bd44333a90df3d8c2a09832056ff529f1a0886
E.8556 <t4nishi.x@gmail.com>
2018-09-27 00:54:02 +0900

    Airクラス追加
    Noteクラスを継承したAirクラスを追加

---
+++ NE4S/Notes/Air.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NE4S.Notes
+{
+	public class Air : Note
+	{
+		public Air()
+		{
+
+		}
+
+
+	}
+}
