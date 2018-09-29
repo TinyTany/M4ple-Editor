0335d9c68d0ab1fad0a74396d0ed9ff52851ff4d
E.8556 <t4nishi.x@gmail.com>
2018-09-29 17:47:48 +0900

    ScoreMaterialBook追加
    今の所使うかはわからないけどScoreMaterialListへの処理が増えてきたら多分分けるように使う

---
+++ NE4S/Scores/ScoreMaterialBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NE4S.Scores
+{
+	public class ScoreMaterialBook : List<ScoreMaterial>
+	{
+		public ScoreMaterialBook()
+		{
+
+		}
+
+		
+	}
+}
