891d74c77dc8bf8bd1561ed736395438fe21529a
E.8556 <t4nishi.x@gmail.com>
2018-10-07 17:57:56 +0900

    LongNoteMaterialクラス追加

---
+++ NE4S/Notes/LongNoteMaterial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NE4S.Notes
+{
+	public class LongNoteMaterial
+	{
+		private List<NoteMaterial> noteList;
+		private Range longNoteRange;
+
+		public LongNoteMaterial(LongNote longNote, Range longNoteRange)
+		{
+			foreach(Note note in longNote)
+			{
+				//noteList.Add(new NoteMaterial(note, ));
+			}
+			this.longNoteRange = longNoteRange;
+		}
+	}
+}
