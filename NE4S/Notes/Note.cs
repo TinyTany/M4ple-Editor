6e48db223382afd662fb5ea85b4f9d61fd02c2f2
E.8556 <t4nishi.x@gmail.com>
2018-10-21 16:29:56 +0900

    Noteクラスに属性追加
    Noteクラスが物理的な属性も持つようになった。ちゃんと責任持って❤

--- NE4S/Notes/Note.cs
+++ NE4S/Notes/Note.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using NE4S.Scores;
 
 namespace NE4S.Notes
 {
@@ -15,36 +16,76 @@ namespace NE4S.Notes
     {
         private int size;
         private Position pos;
+		protected RectangleF hitRect;
 
 		public Note()
 		{
 			size = 0;
 			pos = null;
+			hitRect = new RectangleF();
 		}
 
         public Note(int size, Position pos, PointF location)
         {
 			this.size = size;
 			this.pos = pos;
+			hitRect.Size = new SizeF(ScoreInfo.MinLaneWidth * size, ScoreInfo.NoteHeight);
+			hitRect.Location = location;
+			//描画中にいい感じにハマるように調節する
+			--hitRect.Width; ++hitRect.X; hitRect.Y -= 2;
         }
 
         public int Size
         {
-            get { return this.size; }
-			set { size = value; }
+            get { return size; }
+			//外で変更されるの嫌じゃない？知らんけど
+			//set { size = value; }
         }
 
         public Position Pos
         {
-            get { return this.pos; }
-			set { pos = value; }
+            get { return pos; }
+			//同上
+			//set { pos = value; }
         }
 
-		public virtual void Draw(PaintEventArgs e, RectangleF hitRect)
+		public void ReSize(int size)
 		{
+			this.size = size;
+			return;
+		}
+
+		public void Relocate(Position pos, PointF location)
+		{
+			Relocate(pos);
+			Relocate(location);
+			return;
+		}
+
+		public void Relocate(Position pos)
+		{
+			this.pos = pos;
+			return;
+		}
+
+		public void Relocate(PointF location)
+		{
+			hitRect.Location = location;
+			//描画中にいい感じにハマるように調節する
+			++hitRect.X; hitRect.Y -= 2;
+			return;
+		}
+
+		public virtual void Draw(PaintEventArgs e, int originPosX, int originPosY)
+		{
+			RectangleF drawRect = new RectangleF(
+				hitRect.X - originPosX,
+				hitRect.Y - originPosY,
+				hitRect.Width,
+				hitRect.Height);
 			using (SolidBrush myBrush = new SolidBrush(Color.White))
 			{
-				e.Graphics.FillRectangle(myBrush, hitRect);
+				e.Graphics.FillRectangle(myBrush, drawRect);
 			}
 			return;
 		}
