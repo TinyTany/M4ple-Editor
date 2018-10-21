f70049d0a640c2487235f18f78440477be580586
E.8556 <t4nishi.x@gmail.com>
2018-10-21 16:23:50 +0900

    コンストラクタ追加
    作ってなかったので

--- NE4S/Notes/Air.cs
+++ NE4S/Notes/Air.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,6 @@ namespace NE4S.Notes
 
 		}
 
-
-	}
+        public Air(int size, Position pos, PointF location) : base(size, pos, location) { }
+    }
 }
