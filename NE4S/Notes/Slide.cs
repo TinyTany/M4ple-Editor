701fd47084a9c06c6b520e04a8bd8d562e7b7d57
TinyTany <t4nishi.x@gmail.com>
2018-10-23 21:00:17 +0900

    Slideノーツの中身実装しかけ
    これから本格的にやってく

--- NE4S/Notes/Slide.cs
+++ NE4S/Notes/Slide.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,14 @@ namespace NE4S.Notes
 
         }
 
-		public override void Draw(PaintEventArgs e)
+        public Slide(int size, Position pos, PointF location)
+        {
+            Add(new SlideBegin(size, pos, location));
+            //TODO: posとかlocationとかをいい感じに設定する
+            Add(new SlideEnd(size, pos, location));
+        }
+
+        public override void Draw(PaintEventArgs e)
 		{
 			
 		}
