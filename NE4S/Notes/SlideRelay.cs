865379f4ba3c3ad4d4babbf9c9adcde35b85083a
E.8556 <t4nishi.x@gmail.com>
2018-11-04 17:59:20 +0900

    Draw関数などを追加

--- NE4S/Notes/SlideRelay.cs
+++ NE4S/Notes/SlideRelay.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace NE4S.Notes
 {
@@ -11,6 +14,26 @@ namespace NE4S.Notes
         public SlideRelay()
         {
 
+        }
+
+        public SlideRelay(int size, Position pos, PointF location) : base(size, pos, location) { }
+
+        public override void Draw(PaintEventArgs e, int originPosX, int originPosY)
+        {
+            RectangleF drawRect = new RectangleF(
+                hitRect.X - originPosX,
+                hitRect.Y - originPosY,
+                hitRect.Width,
+                hitRect.Height);
+            using (LinearGradientBrush gradientBrush = new LinearGradientBrush(new PointF(0, drawRect.Y), new PointF(0, drawRect.Y + drawRect.Height), Color.Blue, Color.DarkBlue))
+            {
+                //e.Graphics.FillRectangle(gradientBrush, drawRect);
+                //e.Graphics.FillRectangle(gradientBrush, new RectangleF(100, 100, 100, 500));
+            }
+            using (Pen pen = new Pen(Color.White, 1))
+            {
+                e.Graphics.DrawRectangles(pen, new RectangleF[]{ drawRect });
+            }
         }
     }
 }
