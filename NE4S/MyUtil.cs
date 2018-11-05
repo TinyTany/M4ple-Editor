b2d6a0f5eb2c929a34a1358922ce65fffe843d09
E.8556 <t4nishi.x@gmail.com>
2018-11-05 23:32:40 +0900

    拡張メソッド追加
    PointやPointFのよく使う計算の拡張メソッド追加

--- NE4S/MyUtil.cs
+++ NE4S/MyUtil.cs
@@ -24,5 +24,55 @@ namespace NE4S
             hitRect.Y -= 2;
             return;
         }
-	}
+
+        public static PointF AddX(this PointF pointF, float x)
+        {
+            return new PointF(pointF.X + x, pointF.Y);
+        }
+
+        public static PointF AddY(this PointF pointF, float y)
+        {
+            return new PointF(pointF.X, pointF.Y + y);
+        }
+
+        public static PointF Add(this PointF pointF, float x, float y)
+        {
+            return new PointF(pointF.X + x, pointF.Y + y);
+        }
+
+        public static PointF Add(this PointF pointF, PointF other)
+        {
+            return new PointF(pointF.X + other.X, pointF.Y + other.Y);
+        }
+
+        public static PointF Add(this PointF pointF, Point other)
+        {
+            return new PointF(pointF.X + other.X, pointF.Y + other.Y);
+        }
+
+        public static PointF Sub(this PointF pointF, PointF other)
+        {
+            return new PointF(pointF.X - other.X, pointF.Y - other.Y);
+        }
+
+        public static PointF Sub(this PointF pointF, Point other)
+        {
+            return new PointF(pointF.X - other.X, pointF.Y - other.Y);
+        }
+
+        public static Point Add(this Point point, int x)
+        {
+            return new Point(point.X + x, point.Y);
+        }
+
+        public static Point Add(this Point pointF, Point other)
+        {
+            return new Point(pointF.X + other.X, pointF.Y + other.Y);
+        }
+
+        public static Point Sub(this Point pointF, Point other)
+        {
+            return new Point(pointF.X - other.X, pointF.Y - other.Y);
+        }
+    }
 }
