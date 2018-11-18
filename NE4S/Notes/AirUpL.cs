13e245b77a1ecd8146614a977cea55de3ced8c6e
E.8556 <t4nishi.x@gmail.com>
2018-11-19 00:20:13 +0900

    各種Air系ノーツ実装

--- NE4S/Notes/AirUpL.cs
+++ NE4S/Notes/AirUpL.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace NE4S.Notes
 {
-    public class AirUpL : Air
+    public class AirUpL : AirUpC
     {
         public AirUpL()
         {
@@ -15,5 +17,33 @@ namespace NE4S.Notes
         }
 
         public AirUpL(int size, Position pos, PointF location) : base(size, pos, location) { }
+
+        public override bool Contains(PointF location)
+        {
+            return GetAirPath(0, 0).IsVisible(location);
+        }
+
+        protected override GraphicsPath GetAirPath(int originPosX, int originPosY)
+        {
+            GraphicsPath graphicsPath = base.GetAirPath(originPosX, originPosY);
+            PointF pathLocation = graphicsPath.GetBounds().Location;
+            var mat = new Matrix();
+            mat.Translate(-pathLocation.X, -pathLocation.Y, MatrixOrder.Append);
+            mat.Shear(shearX, 0, MatrixOrder.Append);
+            mat.Translate(pathLocation.X - shearX * (airHeight - borderWidth * 2), pathLocation.Y, MatrixOrder.Append);
+            graphicsPath.Transform(mat);
+            return graphicsPath;
+        }
+
+        public override void Draw(PaintEventArgs e, int originPosX, int originPosY)
+        {
+            using (GraphicsPath graphicsPath = GetAirPath(originPosX, originPosY))
+            using (SolidBrush brush = new SolidBrush(airUpColor))
+            using (Pen pen = new Pen(Color.White, borderWidth))
+            {
+                e.Graphics.DrawPath(pen, graphicsPath);
+                e.Graphics.FillPath(brush, graphicsPath);
+            }
+        }
     }
 }
