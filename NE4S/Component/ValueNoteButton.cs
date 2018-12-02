1b1d19b1a121a561199596ce82e386912a199980
E.8556 <t4nishi.x@gmail.com>
2018-12-02 23:19:47 +0900

    クラス追加
    属性ノーツ用のクラスと、それようのノーツボタンクラス

---
+++ NE4S/Component/ValueNoteButton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NE4S.Component
+{
+    public class ValueNoteButton : NoteButton
+    {
+        public ValueNoteButton(int noteType, NoteButtonEventHandler handler) : base(noteType, handler) { }
+    }
+}
---
+++ NE4S/Notes/Speed.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NE4S.Notes
+{
+    public class Speed : AttributeNote
+    {
+    }
+}
