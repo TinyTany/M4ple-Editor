bb0a127ef6d9e43d32a3f73645a921f87c893ac3
E.8556 <t4nishi.x@gmail.com>
2018-12-12 00:23:06 +0900

    ノーツ描画修正

--- NE4S/Notes/ExTapDown.cs
+++ NE4S/Notes/ExTapDown.cs
@@ -23,25 +23,30 @@ namespace NE4S.Notes
                 noteRect.Y - originPosY + adjustNoteRect.Y,
                 noteRect.Width,
                 noteRect.Height);
-            using (LinearGradientBrush gradientBrush = new LinearGradientBrush(new PointF(0, drawRect.Y), new PointF(0, drawRect.Y + drawRect.Height), Color.Yellow, Color.FromArgb(195, 185, 65)))
+            if (Status.IsExTapDistinct)
             {
-                e.Graphics.FillRectangle(gradientBrush, drawRect);
+                using (LinearGradientBrush gradientBrush = new LinearGradientBrush(new PointF(0, drawRect.Y), new PointF(0, drawRect.Y + drawRect.Height), Color.Yellow, Color.Blue))
+                {
+                    e.Graphics.FillRectangle(gradientBrush, drawRect);
+                }
+            }
+            else
+            {
+                using (LinearGradientBrush gradientBrush = new LinearGradientBrush(new PointF(0, drawRect.Y), new PointF(0, drawRect.Y + drawRect.Height), Color.Yellow, Color.FromArgb(195, 185, 65)))
+                {
+                    e.Graphics.FillRectangle(gradientBrush, drawRect);
+                }
             }
             using (Pen pen = new Pen(Color.White, 1))
             {
                 e.Graphics.DrawLine(pen, new PointF(drawRect.X + 4, drawRect.Y + 2), new PointF(drawRect.X + drawRect.Width - 4, drawRect.Y + 2));
             }
-            using (Pen pinkPen = new Pen(Color.HotPink, 1))
             using (Pen grayPen = new Pen(Color.LightGray, 1))
             {
-                if (Status.IsExTapDistinct)
-                {
-                    e.Graphics.DrawPath(pinkPen, drawRect.RoundedPath());
-                }
-                else
-                {
-                    e.Graphics.DrawPath(grayPen, drawRect.RoundedPath());
-                }
+                e.Graphics.DrawPath(grayPen, drawRect.RoundedPath());
+            }
+        }
+
         public static void Draw(PaintEventArgs e, PointF location, SizeF size)
         {
             RectangleF drawRect = new RectangleF(location.X - size.Width / 2f, location.Y - size.Height / 2f, size.Width, size.Height);
