5864410f20e2fd570b3461558c20e7d88044da8e
E.8556 <t4nishi.x@gmail.com>
2018-12-17 01:40:46 +0900

    エクスポート実装途中

--- NE4S/Component/ExportForm.cs
+++ NE4S/Component/ExportForm.cs
@@ -26,12 +26,27 @@ namespace NE4S.Component
             {
                 streamWriter.WriteLine("This score was created by M4ple Editor.");
 
-                List<Note> shortNotes = noteBook.ShortNotes;
-                streamWriter.WriteLine("ShortNote");
+                /* TODO: この辺に基本情報やBPM,ハイスピの情報を書き出す処理書く */
+
+                streamWriter.WriteLine("\nSet measure's pulse");
                 foreach(Score score in scoreBook)
                 {
+                    if(score == scoreBook.First() || (score.BarSize != scoreBook.Prev(score).BarSize))
+                    {
+                        streamWriter.WriteLine("#" + score.Index.ToString("D3") + "02: " + (4.0 * score.BarSize));
+                    }
+                }
+                streamWriter.WriteLine("\nShortNote");
+                List<Note> shortNotes = noteBook.ShortNotes;
+                foreach (Score score in scoreBook)
+                {
                     List<Note> currentScoreNotes = shortNotes.Where(x => score.StartTick <= x.Position.Tick && x.Position.Tick <= score.EndTick).ToList();
-                    
+                    for(int lane = 0; lane < ScoreInfo.Lanes; ++lane)
+                    {
+                        List<Note> currentLaneNotes = currentScoreNotes.Where(x => x.Position.Lane == lane).ToList();
+                        if (!currentLaneNotes.Any()) continue;
+
+                    }
                 }
             }
             return true;
