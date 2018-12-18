f2b6d7a351bc06242c1d5987842410fcc379f4c9
TinyTany <t4nishi.x@gmail.com>
2018-12-18 14:43:20 +0900

    AirHoldEndクラス追加

---
+++ NE4S/Notes/AirHoldEnd.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NE4S.Notes
+{
+    public class AirHoldEnd : AirAction
+    {
+        public override int NoteID => 2;
+
+        public AirHoldEnd(int size, Position pos, PointF location, int laneIndex) : base(size, pos, location, laneIndex) { }
+    }
+}
