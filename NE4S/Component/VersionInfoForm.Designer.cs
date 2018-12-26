5c76efc9121b5dcab759dca6d8381a7169115d9f
E.8556 <t4nishi.x@gmail.com>
2018-12-27 03:15:45 +0900

    バージョン情報フォーム

---
+++ NE4S/Component/VersionInfoForm.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace NE4S.Component
+{
+    partial class VersionInfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // VersionInfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "VersionInfoForm";
+            this.Text = "バージョン情報";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
