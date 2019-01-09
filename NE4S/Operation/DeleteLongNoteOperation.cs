cd2aa4923c3a3ae3cb6f429945d0a238630e0c4e
E.8556 <t4nishi.x@gmail.com>
2019-01-09 23:26:04 +0900

    DeleteLongNoteOperationクラス追加
    ロングノーツを消す操作オブジェクト（多分完成）

---
+++ NE4S/Operation/DeleteLongNoteOperation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NE4S.Notes;
+
+namespace NE4S.Operation
+{
+    public class DeleteLongNoteOperation : Operation
+    {
+        public DeleteLongNoteOperation(Model model, LongNote longNote)
+        {
+            List<Hold> holdNotes = model.NoteBook.HoldNotes;
+            List<Slide> slideNotes = model.NoteBook.SlideNotes;
+            List<AirHold> airHoldNotes = model.NoteBook.AirHoldNotes;
+            List<Air> airNotes = model.NoteBook.AirNotes;
+            Air attachedAir = null;
+            AirHold attachedAirHold = null;
+
+            Invoke += () =>
+            {
+                if (longNote is Hold) holdNotes.Remove(longNote as Hold);
+                else if (longNote is Slide) slideNotes.Remove(longNote as Slide);
+                else if (longNote is AirHold) airHoldNotes.Remove(longNote as AirHold);
+                //終点にくっついてるかもしれないAir系ノーツの破棄
+                if (longNote?.Find(x => x is AirableNote) is AirableNote airable)
+                {
+                    if (airable.IsAirAttached)
+                    {
+                        airNotes.Remove(airable.Air);
+                        attachedAir = airable.Air;
+                        airable.DetachAir();
+                    }
+                    if (airable.IsAirHoldAttached)
+                    {
+                        airHoldNotes.Remove(airable.AirHold);
+                        attachedAirHold = airable.AirHold;
+                        airable.DetachAirHold();
+                    }
+                }
+            };
+            Undo += () =>
+            {
+                if (longNote is Hold) holdNotes.Add(longNote as Hold);
+                else if (longNote is Slide) slideNotes.Add(longNote as Slide);
+                else if (longNote is AirHold) airHoldNotes.Add(longNote as AirHold);
+                var airable = longNote?.Find(x => x is AirableNote) as AirableNote;
+                if (attachedAir != null)
+                {
+                    airable?.AttachAir(attachedAir);
+                    airNotes.Add(attachedAir);
+                }
+                if (attachedAirHold != null)
+                {
+                    airable?.AttachAirHold(attachedAirHold);
+                    airHoldNotes.Add(attachedAirHold);
+                }
+            };
+        }
+    }
+}
