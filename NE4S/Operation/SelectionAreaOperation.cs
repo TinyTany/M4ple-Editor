c0794844636364f5379573440ec75978a95c9462
E.8556 <t4nishi.x@gmail.com>
2019-02-08 00:14:59 +0900

    コピペ周りのUndo/Redoの改善

--- NE4S/Operation/SelectionAreaOperation.cs
+++ NE4S/Operation/SelectionAreaOperation.cs
@@ -11,10 +11,13 @@ namespace NE4S.Operation
 {
     public class CopyNotesOperation : Operation
     {
+        SelectionArea selectionArea = new SelectionArea();
+
         public CopyNotesOperation(SelectionArea selectionArea)
         {
             Invoke += () =>
             {
+                this.selectionArea.Reset(selectionArea);
                 Clipboard.SetDataObject(selectionArea);
                 //NOTE: どっちにするか（コピー後矩形を保持or破棄）迷う...
                 //selectionArea = new SelectionArea();
@@ -24,16 +27,16 @@ namespace NE4S.Operation
 
     public class CutNotesOperation : Operation
     {
-        SelectionArea selectionArea = new SelectionArea();
+        SelectionArea selectionArea;
 
         public CutNotesOperation(Model model, SelectionArea selectionArea)
         {
+            this.selectionArea = new SelectionArea(selectionArea);
+
             Invoke += () =>
             {
-                this.selectionArea.Reset(selectionArea);
-                new CopyNotesOperation(selectionArea).Invoke();
+                new CopyNotesOperation(this.selectionArea).Invoke();
                 new ClearAreaNotesOperation(model, selectionArea).Invoke();
-                selectionArea.Reset();
             };
             Undo += () =>
             {
@@ -48,20 +51,50 @@ namespace NE4S.Operation
 
     public class PasteNotesOperation : Operation
     {
+        SelectionArea selectionArea = null;
+
+        /// <summary>
+        /// Clipboardのデータから貼り付けを行います
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="selectionArea"></param>
+        /// <param name="position"></param>
         public PasteNotesOperation(Model model, SelectionArea selectionArea, Position position)
         {
+            if (Clipboard.GetDataObject().GetData(typeof(SelectionArea)) is SelectionArea data)
+            {
+                this.selectionArea = data;
+            }
+
             Invoke += () =>
             {
-                if (Clipboard.GetDataObject().GetData(typeof(SelectionArea)) is SelectionArea data)
+                if (this.selectionArea == null) return;
+                selectionArea.Reset(this.selectionArea);
+                selectionArea.MovePositionDelta = new Position();
+                foreach (Note note in selectionArea.SelectedNoteList)
                 {
-                    selectionArea.Reset(data);
-                    selectionArea.MovePositionDelta = new Position();
-                    foreach (Note note in selectionArea.SelectedNoteList)
+                    model.NoteBook.Add(note);
+                    if (note is AirableNote)
                     {
-                        model.NoteBook.Add(note);
-                        if (note is AirableNote)
+                        AirableNote airable = note as AirableNote;
+                        if (airable.IsAirAttached)
                         {
-                            AirableNote airable = note as AirableNote;
+                            model.NoteBook.Add(airable.Air);
+                        }
+                        if (airable.IsAirHoldAttached)
+                        {
+                            model.NoteBook.Add(airable.AirHold);
+                        }
+                    }
+                }
+                foreach (LongNote longNote in selectionArea.SelectedLongNoteList)
+                {
+                    model.NoteBook.Add(longNote);
+                    longNote.ForEach(x =>
+                    {
+                        if (x is AirableNote)
+                        {
+                            AirableNote airable = x as AirableNote;
                             if (airable.IsAirAttached)
                             {
                                 model.NoteBook.Add(airable.Air);
@@ -71,31 +104,14 @@ namespace NE4S.Operation
                                 model.NoteBook.Add(airable.AirHold);
                             }
                         }
-                    }
-                    foreach (LongNote longNote in selectionArea.SelectedLongNoteList)
-                    {
-                        model.NoteBook.Add(longNote);
-                        longNote.ForEach(x =>
-                        {
-                            if (x is AirableNote)
-                            {
-                                AirableNote airable = x as AirableNote;
-                                if (airable.IsAirAttached)
-                                {
-                                    model.NoteBook.Add(airable.Air);
-                                }
-                                if (airable.IsAirHoldAttached)
-                                {
-                                    model.NoteBook.Add(airable.AirHold);
-                                }
-                            }
-                        });
-                    }
-                    selectionArea.Relocate(position, model.LaneBook);
+                    });
                 }
+                selectionArea.Relocate(position, model.LaneBook);
             };
             Undo += () =>
             {
+                // TODO: Undo後Redoを繰り返すとノーツが再配置される位置がずれていくのでそれを改善する
+                //       多分矩形の位置変更をUndoしてないせい
                 new ClearAreaNotesOperation(model, selectionArea).Invoke();
             };
         }
@@ -103,30 +119,37 @@ namespace NE4S.Operation
 
     public class PasteAndReverseNotesOperation : Operation
     {
+        PasteNotesOperation paste;
+        ReverseNotesOperation reverse;
+
         public PasteAndReverseNotesOperation(Model model, SelectionArea selectionArea, Position position)
         {
+            paste = new PasteNotesOperation(model, selectionArea, position);
+            reverse = new ReverseNotesOperation(model, selectionArea);
+
             Invoke += () =>
             {
-                new ReverseNotesOperation(model, selectionArea).Invoke();
-                new PasteNotesOperation(model, selectionArea, position).Invoke();
+                paste.Invoke();
+                reverse.Invoke();
             };
             Undo += () =>
             {
-                new ReverseNotesOperation(model, selectionArea).Undo();
-                new PasteNotesOperation(model, selectionArea, position).Undo();
+                reverse.Undo();
+                paste.Undo();
             };
         }
     }
 
     public class ClearAreaNotesOperation : Operation
     {
-        SelectionArea selectionArea = new SelectionArea();
+        SelectionArea selectionArea;
 
         public ClearAreaNotesOperation(Model model, SelectionArea selectionArea)
         {
+            this.selectionArea = new SelectionArea(selectionArea);
+
             Invoke += () =>
             {
-                this.selectionArea.Reset(selectionArea);
                 selectionArea.ClearAllNotes(model.NoteBook);
                 selectionArea.Reset();
             };
