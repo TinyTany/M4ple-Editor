b337f1be60e46bf11366e8d9dcfbf09ff31860f5
E.8556 <t4nishi.x@gmail.com>
2019-02-28 03:01:39 +0900

    Operation追加
    Slideの切断と結合

--- NE4S/Operation/EditNoteOperation.cs
+++ NE4S/Operation/EditNoteOperation.cs
@@ -118,6 +118,9 @@ namespace NE4S.Operation
         }
     }
 
+    /// <summary>
+    /// Slideを指定の分数ごとでTick方向にSlideTapまたはSldeRelayで分割します
+    /// </summary>
     public class DivideSlideOperation : Operation
     {
         public DivideSlideOperation(Slide slide, Note stepPast, Note stepFuture)
@@ -133,5 +136,111 @@ namespace NE4S.Operation
                 // UNDONE
             };
         }
+    }
+
+    /// <summary>
+    /// 指定したSlide帯でSlideを2つに切断します
+    /// </summary>
+    public class CutSlideOperation : Operation
+    {
+        public CutSlideOperation(Model model, Slide slide, Note past, Note future)
+        {
+            #region 分割した手前側のSlideノーツを生成
+            Slide before = new Slide();
+            foreach(var note in slide.Where(x => x.Position.Tick < past.Position.Tick))
+            {
+                before.Add(note);
+            }
+            before.Add(new Note(past) as SlideEnd);
+            #endregion
+            #region 分割した奥側のSlideノーツを生成
+            Slide after = new Slide();
+            after.Add(new Note(future) as SlideBegin);
+            foreach (var note in slide.Where(x => x.Position.Tick > future.Position.Tick))
+            {
+                after.Add(note);
+            }
+            #endregion
+            Invoke += () =>
+            {
+#if DEBUG
+                if (!slide.Any() || !before.Any() || !after.Any())
+                {
+                    System.Diagnostics.Debug.Assert(false, "空のSlideが存在します。");
+                    return;
+                }
+#endif
+                model.NoteBook.Delete(slide);
+                model.NoteBook.Add(before);
+                model.NoteBook.Add(after);
+            };
+            Undo += () =>
+            {
+#if DEBUG
+                if (!slide.Any() || !before.Any() || !after.Any())
+                {
+                    System.Diagnostics.Debug.Assert(false, "空のSlideが存在します。");
+                    return;
+                }
+#endif
+                model.NoteBook.Add(slide);
+                model.NoteBook.Delete(before);
+                model.NoteBook.Delete(after);
+            };
+        }
+    }
+    
+    /// <summary>
+    /// 指定した2つのSlideを1つのSlideへ結合します。
+    /// 2つのSlideは時間順的に重なっていないことを前提とする。
+    /// </summary>
+    public class ConnectSlideOperation : Operation
+    {
+        /// <summary>
+        /// 2つのSlideは時間順的に正しくなるように与えられていることを前提とする。
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="past"></param>
+        /// <param name="future"></param>
+        public ConnectSlideOperation(Model model, Slide past, Slide future)
+        {
+            #region 結合したSlide（union）を作成
+            Slide union = new Slide();
+            var end = past.Find(x => x is SlideEnd);
+            System.Diagnostics.Debug.Assert(end != null, "Slide終点が見つかりません。");
+            foreach(var note in past.Where(x => x != end))
+            {
+                union.Add(note);
+            }
+            union.Add(new Note(end) as SlideTap);
+            var begin = future.Find(x => x is SlideBegin);
+            System.Diagnostics.Debug.Assert(begin != null, "Slide始点が見つかりません。");
+            union.Add(new Note(begin) as SlideTap);
+            foreach(var note in future.Where(x => x != begin))
+            {
+                union.Add(note);
+            }
+            #endregion
+            Invoke += () =>
+            {
+                model.NoteBook.Add(union);
+                model.NoteBook.Delete(past);
+                model.NoteBook.Delete(future);
+            };
+            Undo += () =>
+            {
+                model.NoteBook.Add(past);
+                model.NoteBook.Add(future);
+                model.NoteBook.Delete(union);
+            };
+        }
+    }
+
+    /// <summary>
+    /// あるSlideノーツの指定した範囲のノーツのパターンを複製します
+    /// </summary>
+    public class ReplicateSlidePatternOperation : Operation
+    {
+
     }
 }
