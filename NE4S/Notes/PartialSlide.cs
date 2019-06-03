d60ebe1d2ca4eefed9ff62bfd5bfd1f91b367e48
E.8556 <t4nishi.x@gmail.com>
2019-06-04 00:19:38 +0900

    PartialSlideクラス追加

---
+++ NE4S/Notes/PartialSlide.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NE4S.Notes
+{
+    /// <summary>
+    /// あるSlideノーツの連続な部分Slideを表します
+    /// </summary>
+    public class PartialSlide
+    {
+        public Slide Slide { get; private set; } = null;
+        public List<Note> Partial { get; private set; } = new List<Note>();
+
+        public PartialSlide(Slide slide, Note start, Note end)
+        {
+            if (slide == null)
+            {
+                System.Diagnostics.Debug.Assert(false, "引数のslideがnullです");
+                return;
+            }
+            Slide = slide;
+            if (!slide.Contains(start) || !slide.Contains(end))
+            {
+                System.Diagnostics.Debug.Assert(false, "引数のnoteが不正です");
+                return;
+            }
+            Partial.AddRange(
+                slide.Where(
+                    x => 
+                    start.Position.Tick <= x.Position.Tick && x.Position.Tick <= end.Position.Tick));
+        }
+    }
+}
