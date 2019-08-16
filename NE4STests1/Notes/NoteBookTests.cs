a3673675ce013269db2a075ac65557aebde2de98
E.8556 <t4nishi.x@gmail.com>
2019-08-17 00:15:08 +0900

    オブジェクトコピーのベンチマーク用コード追加

--- NE4STests1/Notes/NoteBookTests.cs
+++ NE4STests1/Notes/NoteBookTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace NE4S.Notes.Tests
 {
@@ -23,6 +24,81 @@ namespace NE4S.Notes.Tests
             Assert.AreEqual(true, book.Contains(airable));
             var note = airable as Note;
             Assert.AreEqual(true, book.Contains(note));
+
+            var slide = new Slide();
+            slide.Add(new SlideBegin());
+            slide.Add(new SlideEnd());
+            var rnd = new Random();
+            for (int i = 0; i < 100; ++i)
+            {
+                switch (rnd.Next() % 3)
+                {
+                    case 0: slide.Add(new SlideTap()); break;
+                    case 1: slide.Add(new SlideRelay()); break;
+                    case 2: slide.Add(new SlideCurve()); break;
+                    default: break;
+                }
+            }
+            var sw = new Stopwatch();
+            {
+                sw.Start();
+                var copy = new Slide(slide);
+                sw.Stop();
+                Debug.WriteLine($"Copy by constructor(Slide) : {sw.ElapsedMilliseconds} [ms]");
+            }
+            sw.Reset();
+            {
+                sw.Start();
+                var copy = MyUtil.DeepCopy(slide);
+                sw.Stop();
+                Debug.WriteLine($"Copy by method(Slide) : {sw.ElapsedMilliseconds} [ms]");
+            }
+            // コピーコンストラクタの方法よりもディープコピーの方法のほうがだいたい10倍くらい早い（型を調べてるから当然ではあるけど）
+
+            var target = new List<Position>();
+            for(int i = 0; i < 10000; ++i)
+            {
+                target.Add(new Position(rnd.Next(), rnd.Next()));
+            }
+            sw.Reset();
+            {
+                sw.Start();
+                var copy = new List<Position>(target);
+                sw.Stop();
+                Debug.WriteLine($"Copy by constructor(List<Position>) : {sw.ElapsedMilliseconds} [ms]");
+            }
+            sw.Reset();
+            {
+                sw.Start();
+                var copy = MyUtil.DeepCopy(target);
+                sw.Stop();
+                Debug.WriteLine($"Copy by method(List<Position>) : {sw.ElapsedMilliseconds} [ms]");
+            }
+            // こっちだとディープコピーのほうが普通に遅い（Listのコンストラクタのほうは要素数10000でも0ms）
+
+            sw.Reset();
+            {
+                sw.Start();
+                foreach(var item in target)
+                {
+                    var copy = new Position(item);
+                }
+                sw.Stop();
+                Debug.WriteLine($"Copy by constructor(Position) : {sw.ElapsedMilliseconds} [ms]");
+            }
+            sw.Reset();
+            {
+                sw.Start();
+                foreach (var item in target)
+                {
+                    var copy = MyUtil.DeepCopy(item);
+                }
+                sw.Stop();
+                Debug.WriteLine($"Copy by method(Position) : {sw.ElapsedMilliseconds} [ms]");
+            }
+            // これもコピーコンストラクタのほうが早い
+
+            // 基本はコピーコンストラクタを使い、型を調べる必要があるならディープコピーのほうが良い？
         }
     }
 }
