a486d09b71321000c255d7ac1cbf733b3fe63dec
E.8556 <t4nishi.x@gmail.com>
2019-11-13 02:16:05 +0900

    DisplayPanelBoxクラスの追加
    イベント振り分けハブ的なものにしようかなというお気持ち

---
+++ NE4S/Component/DisplayPanelBox.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NE4S.Component
+{
+    public class DisplayPanelBox
+    {
+        public event Action<MouseEventArgs> MouseDown, MouseUp, MouseMove, MouseDrag;
+        private readonly PictureBox pictureBox;
+        private readonly VScrollBar vScroll;
+        private readonly HScrollBar hScroll;
+        private bool clicking = false;
+
+        public DisplayPanelBox(PictureBox pBox, VScrollBar vs, HScrollBar hs)
+        {
+            pictureBox = pBox;
+            pictureBox.MouseDown += (s, e) =>
+            {
+                clicking = true;
+                MouseDown.Invoke(e);
+            };
+            pictureBox.MouseMove += (s, e) =>
+            {
+                if (clicking)
+                {
+                    MouseDrag.Invoke(e);
+                }
+                else
+                {
+                    MouseMove.Invoke(e);
+                }
+            };
+            pictureBox.MouseUp += (s, e) =>
+            {
+                clicking = false;
+                MouseUp.Invoke(e);
+            };
+            vScroll = vs;
+            hScroll = hs;
+        }
+    }
+}
