5f78b721e9e3ad7b4caed9c8473ce86410b5731f
E.8556 <t4nishi.x@gmail.com>
2019-11-13 02:16:41 +0900

    各種ノーツデータようインターフェース追加
    内部構造をちゃんと整理したいわねの一環

---
+++ NE4S/Notes/Interface/IAttributeNote.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NE4S.Notes.Interface
+{
+    public interface IAttributeNote : INote
+    {
+        //decimal Value { get; set; }
+    }
+}
---
+++ NE4S/Notes/Interface/INote.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NE4S.Notes.Interface
+{
+    public interface INote
+    {
+        //int ID { get; }
+        //int Size { get; set; }
+        //Position Position { get; set; }
+    }
+}
