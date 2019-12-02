11aaf65cb860fe695355530c1ae3983cb8bce250
E.8556 <t4nishi.x@gmail.com>
2019-12-03 02:17:42 +0900

    NoteBoxクラス追加

---
+++ NE4S/Notes/NoteBox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NE4S.Notes.Abstract;
+using System.Drawing;
+
+namespace NE4S.Notes
+{
+    /// <summary>
+    /// データのみのノーツクラスを物体化するクラス
+    /// </summary>
+    public class NoteBox
+    {
+        private readonly Note note;
+
+        private RectangleF rectangle;
+
+        private NoteBox() { }
+
+        public NoteBox(Note note)
+        {
+            this.note = note;
+        }
+    }
+}
