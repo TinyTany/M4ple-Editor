7e141747161b935b252cb93651b80b39ea5c86c2
E.8556 <t4nishi.x@gmail.com>
2020-04-09 00:16:55 +0900

    IExchanger追加
    各種譜面データの入出力関数用インターフェース

---
+++ NE4S/Exchange/IExchanger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NE4S.Exchange
+{
+    public interface IExchanger<T>
+    {
+        /// <summary>
+        /// 外部IO（ファイルなど）からのデータを入力として、T型のデータ構造を構築します
+        /// </summary>
+        bool Import(out T t);
+
+        /// <summary>
+        /// T型のデータ構造を、外部IO（ファイルなど）に出力します
+        /// </summary>
+        bool Export(in T t);
+    }
+
+    public interface IExchanger<T, U>
+    {
+        /// <summary>
+        /// T型のデータ構造を、U型のデータ構造に変換します
+        /// </summary>
+        bool Transpile(in T t, out U u);
+
+        /// <summary>
+        /// U型のデータ構造を、T型のデータ構造に変換します
+        /// </summary>
+        bool Transpile(in U u, out T t);
+    }
+}
